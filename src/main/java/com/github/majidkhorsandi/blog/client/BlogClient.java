2f7a787aa5bd608b619102524f9200369b804630
Majid Khorsandi <majid.khorsandi@tink.se>
2019-12-02 00:02:32 +0100

    feat: Add service skeleton

---
+++ src/main/java/com/github/majidkhorsandi/blog/client/BlogClient.java
@@ -0,0 +1,8 @@
+package com.github.majidkhorsandi.blog.client;
+
+public class BlogClient {
+
+  public static void main(String[] args) {
+    System.out.println("gRPC client for the blog service");
+  }
+}
---
+++ src/main/java/com/github/majidkhorsandi/blog/server/BlogServiceImpl.java
@@ -0,0 +1,6 @@
+package com.github.majidkhorsandi.blog.server;
+
+import com.proto.blog.BlogServiceGrpc;
+
+public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
+}
