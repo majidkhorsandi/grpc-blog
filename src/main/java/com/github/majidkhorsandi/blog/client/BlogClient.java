393dd6dd79bfce674dc5dd17305df29b415fb2ee
Majid Khorsandi <majid.khorsandi@tink.se>
2019-12-02 00:40:45 +0100

    feat: new blog insertion logic

--- src/main/java/com/github/majidkhorsandi/blog/client/BlogClient.java
+++ src/main/java/com/github/majidkhorsandi/blog/client/BlogClient.java
@@ -1,8 +1,35 @@
 package com.github.majidkhorsandi.blog.client;
 
+import com.proto.blog.Blog;
+import com.proto.blog.BlogServiceGrpc;
+import com.proto.blog.CreateBlogRequest;
+import com.proto.blog.CreateBlogResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class BlogClient {
 
   public static void main(String[] args) {
-    System.out.println("gRPC client for the blog service");
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+            .usePlaintext()
+            .build();
+
+    BlogServiceGrpc.BlogServiceBlockingStub stub = BlogServiceGrpc.newBlockingStub(channel);
+
+    Blog blog = Blog.newBuilder()
+            .setAuthorId("majid")
+            .setContent("Hello. This is my first post")
+            .setTitle("First Post")
+            .build();
+
+    CreateBlogRequest request = CreateBlogRequest.newBuilder()
+            .setBlog(blog)
+            .build();
+
+    CreateBlogResponse blogResponse = stub.createBlog(request);
+
+    System.out.println("created a new blog with id: " + blogResponse.getBlog().getId());
+    System.out.println("created a new blog with title: " + blogResponse.getBlog().getTitle());
+
   }
 }
